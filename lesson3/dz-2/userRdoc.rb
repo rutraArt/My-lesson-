6ffbcfff22a85388ffeecf76b9aeebeb62ca293d
rutraArt <everinartdik@gmail.com>
2021-06-23 19:01:14 +0300

    dz 3.2

---
+++ lesson3/dz-2/userRdoc.rb
@@ -0,0 +1,21 @@
+class User
+  ##
+  # Выводит в стандартный вывод данные User
+  # 
+  def fio
+    'Чабан Артур Валерьевич'
+  end
+
+  def profession
+    'Менеджер'
+  end
+
+  def age
+    '34 года'
+  end
+end
+
+user = User.new
+p user.fio # Тут ФИО специально не добовлял.
+p 'Профессия: ' + user.profession
+p 'Полных лет: ' + user.age
