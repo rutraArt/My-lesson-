b63a487941bfc5fa15c2b321740ccaba25721780
rutraArt <everinartdik@gmail.com>
2021-06-24 22:30:38 +0300

    dz4

---
+++ lesson4/dz4.5.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+##
+# Я не понимаю. Так,или это слишком просто ?
+
+' hello world '
+
+space = ' hello world '
+
+# puts space.size
+
+p space[1..11]
---
+++ lesson4/dz4.6.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Больше чем уверен что не так, но я не понимаю как нужно это делать!
+
+puts 'Hello, ', 'World!'
+
+print 'Hello, '
+puts 'World!'
