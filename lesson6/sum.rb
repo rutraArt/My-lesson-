8d8b2004fc7c50d0c541bd577d58a797010de01b
rutraArt <everinartdik@gmail.com>
2021-06-29 23:57:21 +0300

    HomeWork6END

--- lesson6/sum.rb
+++ lesson6/sum.rb
@@ -1,48 +1,11 @@
 # frozen_string_literal: true
 
-# sum = [6, 3, 7, 12, 2]
-# puts sum.reduce(:+)
+# До rubocop:
+STDIN.gets.chomp
+summ = ARGV.map { |e| e.to_i}
 
-# k = ARGV([0], [0], [0], [0], [0]).to_i
-# if k != 5
-
-#   puts k
-#     .exit
-# # end
-
-# sun = ARGV
-# # num = 5
-# # while sun <=> num do
-# #   puts sun.sum
-# # end
-
-# # sim = sun.sum
-
-# number = ARGV[0].to_i.map
-# def number (number)
-#   number.sum
-# end
-
-# puts number
-
-# one = ARGV.sum
-
-# # def one
-# #   one.length = 5
-# # end
-
-# # onq = one.map!
-# # puts one.class
-# puts one.to_i
-
-# one = ARGV[0].to_i
-# two = ARGV[1].to_i
-# three = ARGV[2].to_i
-# four = ARGV[3].to_i
-# five = ARGV[4].to_i
-
-# puts one + two + three + four + five
+print 'Вывести сумму введенных чисел (Press ENTER): '
+$stdin.gets.chomp
 
 summ = ARGV.map(&:to_i)
-
 puts summ.sum
