0a224661d329a75ad5b3ecd29dfa1fc845a852fb
rutraArt <everinartdik@gmail.com>
2021-06-29 21:16:53 +0300

    HomeWork6

--- lesson6/sum.rb
+++ lesson6/sum.rb
@@ -1,10 +1,44 @@
 # frozen_string_literal: true
 
-sum = [6, 3, 7, 12, 2]
-puts sum.reduce(:+)
+# sum = [6, 3, 7, 12, 2]
+# puts sum.reduce(:+)
 
 # k = ARGV([0], [0], [0], [0], [0]).to_i
 # if k != 5
+
 #   puts k
 #     .exit
+# # end
+
+# sun = ARGV
+# # num = 5
+# # while sun <=> num do
+# #   puts sun.sum
+# # end
+
+# # sim = sun.sum
+
+# number = ARGV[0].to_i.map
+# def number (number)
+#   number.sum
 # end
+
+# puts number
+
+# one = ARGV.sum
+
+# # def one
+# #   one.length = 5
+# # end
+
+# # onq = one.map!
+# # puts one.class
+# puts one.to_i
+
+one = ARGV[0].to_i
+two = ARGV[1].to_i
+three = ARGV[2].to_i
+four = ARGV[3].to_i
+five = ARGV[4].to_i
+
+puts one + two + three + four + five
